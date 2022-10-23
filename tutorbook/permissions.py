81744071beac7e49d6a7f225cddcb9dedb8fe82d
Will <willjcGA@outlook.com>
2022-10-23 20:36:03 +0800

    feat: addded thread detail permissions

--- tutorbook/permissions.py
+++ tutorbook/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from .models import User, Assignment
+from .models import Tutor
 
 class IsOwner(permissions.BasePermission):
     """
@@ -7,12 +7,17 @@ class IsOwner(permissions.BasePermission):
     """
     def has_object_permission(self, request, view, obj):
         has_permission = False
+        try:
+            tutor = Tutor.objects.get(user_id = request.user.pk)
+        except:
+            pass
+        print('tutor.pk', tutor.pk)
         print('obj.user_id: ', obj.user_id)
         print('request.user.pk: ', request.user.pk)
         print('obj.tutor_id: ', obj.tutor_id)
-        # Test if user_id on the object is the as user_id of the request
+        # Test if user_id on the object is the as user_id of the request or that the tutor_id matches the tutor id of the user
         try:
-            has_permission = obj.user_id == request.user.pk or obj.tutor_id == request.user.pk
+            has_permission = obj.user_id == request.user.pk or obj.tutor_id == tutor.pk
         except Exception as error:
             print(error)
             pass
